ee07039c6781e21e198ea9c560fc9748eed9e0ff
learn <monicat.uml2@gmail.com>
2024-06-06 14:11:17 +0800

    feat:消息轉換器HttpMessageConverter
    消息轉換器HttpMessageConverter
    
    Learn form #"spring boot + vue3"

--- src/main/java/ltd/newbee/mall/controller/TestController.java
+++ src/main/java/ltd/newbee/mall/controller/TestController.java
@@ -2,6 +2,9 @@ package ltd.newbee.mall.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
@@ -12,5 +15,13 @@ public class TestController {
         System.out.println("weight:" + weight);
         System.out.println("type:" + type);
         System.out.println("onSale:" + onSale);
+    }
+
+    @RequestMapping(value = "/test/httpmessageconverter", method = RequestMethod.POST)
+    public SaleGoods httpMessageConverterTest(@RequestBody SaleGoods saleGoods) {
+        System.out.println(saleGoods.toString());
+        saleGoods.setType(saleGoods.getType() + 1);
+        saleGoods.setGoodsName("商品名:" + saleGoods.getGoodsName());
+        return saleGoods;
     }
 }
