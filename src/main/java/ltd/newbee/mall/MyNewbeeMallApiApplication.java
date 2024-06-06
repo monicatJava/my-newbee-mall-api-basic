6a831c026947c311481383e3d37cf7b699d306f2
learn <monicat.uml2@gmail.com>
2024-06-06 13:15:18 +0800

    使用Spring initializer 初始話一個Spring boot項目

---
+++ src/main/java/ltd/newbee/mall/MyNewbeeMallApiApplication.java
@@ -0,0 +1,13 @@
+package ltd.newbee.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewbeeMallApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNewbeeMallApiApplication.class, args);
+	}
+
+}
